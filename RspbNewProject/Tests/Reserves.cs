a500202eef4e22d9fee0c49fd0c86f53567b8527
Sushma Nagendranath <Sushma.Nagendranath@RSPB.ORG.UK>
2023-07-25 15:50:17 +0100

    Reserves Opening hours

--- RspbNewProject/Tests/Reserves.cs
+++ RspbNewProject/Tests/Reserves.cs
@@ -52,6 +52,48 @@ namespace RspbNewProject
                 }
             }
         }
+        //Reserve Hours
+        //Hours
+
+        [Test, Category("Smoke Testing")]
+        public void ReserveHoursPage()
+        {
+            try
+            {
+                IWebElement dropdown = driver.FindElement(By.XPath("//a[normalize-space()='Days out']"));
+                Actions action = new Actions(driver);
+                action.MoveToElement(dropdown).Perform();
+                action.MoveToElement(driver.FindElement(By.XPath("//a[normalize-space()='Visit a reserve']"))).Click().Perform();
+                action.MoveToElement(driver.FindElement(By.XPath("//a[@href='/days-out/reserves/arne']"))).Click().Perform();
+                Thread.Sleep(5000);
+                action.MoveToElement(driver.FindElement(By.XPath("//a[@href='/days-out/reserves/arne/opening-times']"))).Click().Perform();
+                Thread.Sleep(5000);
+                Assert.True(driver.FindElement(By.CssSelector("h1[class='title']")).Text.Contains("Opening times"));
+                string content = driver.FindElement(By.CssSelector("h1[class='title']")).Text;
+                Assert.IsTrue(content.Contains("Opening times"));
+                Thread.Sleep(5000);
+                driver.Close();
+            }
+            catch (Exception e)
+            {
+                ITakesScreenshot tr = driver as ITakesScreenshot;
+                Screenshot screenshot = tr.GetScreenshot();
+                screenshot.SaveAsFile("C:\\Users\\sushmanagendranath\\source\\repos\\RspbNewProject\\RspbNewProject\\Screenshots\\Screenshot.jpeg", ScreenshotImageFormat.Jpeg);
+                Console.WriteLine(e.StackTrace);
+                throw;
+            }
+            finally
+            {
+                if (driver != null)
+                {
+                    driver.Quit();
+                }
+            }
+        }
+
+        //Hours end
+
+        //End Reserve hours
 
     }
 }
