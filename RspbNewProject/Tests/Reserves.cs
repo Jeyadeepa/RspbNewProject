bd8d8a1074d70eb838603f47d507470988837d02
Sushma Nagendranath <Sushma.Nagendranath@RSPB.ORG.UK>
2023-07-25 17:03:07 +0100

    Reserves Whats here page

--- RspbNewProject/Tests/Reserves.cs
+++ RspbNewProject/Tests/Reserves.cs
@@ -52,8 +52,7 @@ namespace RspbNewProject
                 }
             }
         }
-        //Reserve Hours
-        //Hours
+        //Reserve Hours   
 
         [Test, Category("Smoke Testing")]
         public void ReserveHoursPage()
@@ -90,10 +89,43 @@ namespace RspbNewProject
                 }
             }
         }
-
-        //Hours end
-
         //End Reserve hours
-
+        //Start Whats here
+        [Test, Category("Smoke Testing")]
+        public void ReserveWhatsHerePage()
+        {
+            try
+            {
+                IWebElement dropdown = driver.FindElement(By.XPath("//a[normalize-space()='Days out']"));
+                Actions action = new Actions(driver);
+                action.MoveToElement(dropdown).Perform();
+                action.MoveToElement(driver.FindElement(By.XPath("//a[normalize-space()='Visit a reserve']"))).Click().Perform();
+                action.MoveToElement(driver.FindElement(By.XPath("//a[@href='/days-out/reserves/arne']"))).Click().Perform();
+                Thread.Sleep(5000);
+                action.MoveToElement(driver.FindElement(By.XPath("//a[@href='/days-out/reserves/arne/facilities']"))).Click().Perform();
+                Thread.Sleep(5000);
+                Assert.True(driver.FindElement(By.CssSelector("h1[class='title']")).Text.Contains("What's here"));
+                string content = driver.FindElement(By.CssSelector("h1[class='title']")).Text;
+                Assert.IsTrue(content.Contains("What's here"));
+                Thread.Sleep(5000);
+                driver.Close();
+            }
+            catch (Exception e)
+            {
+                ITakesScreenshot tr = driver as ITakesScreenshot;
+                Screenshot screenshot = tr.GetScreenshot();
+                screenshot.SaveAsFile("C:\\Users\\sushmanagendranath\\source\\repos\\RspbNewProject\\RspbNewProject\\Screenshots\\Screenshot.jpeg", ScreenshotImageFormat.Jpeg);
+                Console.WriteLine(e.StackTrace);
+                throw;
+            }
+            finally
+            {
+                if (driver != null)
+                {
+                    driver.Quit();
+                }
+            }
+        }
+        //End Whats here
     }
 }
