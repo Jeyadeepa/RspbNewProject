8361b061a83d5cb3be3d19c190286ea3de80451d
Sushma Nagendranath <Sushma.Nagendranath@RSPB.ORG.UK>
2023-07-28 14:42:47 +0100

    Reserves search results page

--- RspbNewProject/Tests/Reserves.cs
+++ RspbNewProject/Tests/Reserves.cs
@@ -200,5 +200,40 @@ namespace RspbNewProject
                 }
             }
         }
+
+        //Reserve Overview
+        [Test, Category("Smoke Testing")]
+        public void ReserveSearchResultsPage()
+        {
+            try
+            {
+                IWebElement dropdown = driver.FindElement(By.XPath("//a[normalize-space()='Days out']"));
+                Actions action = new Actions(driver);
+                action.MoveToElement(dropdown).Perform();
+                action.MoveToElement(driver.FindElement(By.XPath("//a[normalize-space()='Visit a reserve']"))).Click().Perform();
+                //action.MoveToElement(driver.FindElement(By.XPath("//a[@href='/days-out/reserves/arne']"))).Click().Perform();
+                //Thread.Sleep(5000);
+                Assert.True(driver.FindElement(By.CssSelector("div span[class='results-title']")).Text.Contains("results"));
+                string content = driver.FindElement(By.CssSelector("div span[class='results-title']")).Text;
+                Assert.IsTrue(content.Contains("results"));
+                Thread.Sleep(5000);
+                driver.Close();
+            }
+            catch (Exception e)
+            {
+                ITakesScreenshot tr = driver as ITakesScreenshot;
+                Screenshot screenshot = tr.GetScreenshot();
+                screenshot.SaveAsFile("C:\\Users\\sushmanagendranath\\source\\repos\\RspbNewProject\\RspbNewProject\\Screenshots\\Screenshot.jpeg", ScreenshotImageFormat.Jpeg);
+                Console.WriteLine(e.StackTrace);
+                throw;
+            }
+            finally
+            {
+                if (driver != null)
+                {
+                    driver.Quit();
+                }
+            }
+        }
     }
 }
